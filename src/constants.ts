ad696993866e56f56485b56e36ce01f5c0289f77
dEvBranco <emfbranco@gmail.com>
2024-12-22 22:42:33 +0000

    save01:37.12

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const SITE_TITLE = 'People Tech Blog';
-export const SITE_DESCRIPTION = 'Articles and tutorials from tech people';
-export const HOME_PAGE_ARTICLE_LIMIT = 7;
+export const SITE_TITLE = "People Tech Blog";
+export const SITE_DESCRIPTION = "Articles and tutorials from tech people";
+export const HOME_PAGE_ARTICLE_LIMIT = 6;
 export const ARTICLES_PER_PAGE = 6;
---
+++ src/utils.ts
@@ -0,0 +1,11 @@
+function formateDate(date: Date): string {
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  return new Date(date).toLocaleDateString(undefined, options);
+}
+
+export { formateDate };
